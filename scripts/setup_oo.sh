0d610c696ed8b53e9efc8daa9f6617e0e1f4ce78
Albert <sero.albert@gmail.com>
2018-07-25 19:36:23 -0700

    Add setup_oo.sh

---
+++ scripts/setup_oo.sh
@@ -0,0 +1,27 @@
+function main {
+  local _cwd=$(pwd);
+  local _USER=$1;
+  local _PASS=$2;
+
+  local _NEXT_LINK="https://api.bitbucket.org/2.0/repositories/ooapi?pagelen=100";
+
+  mkdir -p /var/oo;
+  cd /var/oo;
+
+  while [ ! -z "$_NEXT_LINK" ]; do
+    local _text=$(curl -u $_USER:$_PASS $_NEXT_LINK | sed -E 's/}|]|,/\
+      /g');
+    local _repos=$(echo "${_text}" | grep -o "git@.*\.git");
+    echo "$_text" > ./data
+    _NEXT_LINK=$(echo "${_text}" | grep -oE "\"next\": \"https:\/\/api.bitbucket.org\/2.0\/repositories\/ooapi.*page=[0-9]+" | grep -oE "https:\/\/api.bitbucket.org\/2.0\/repositories\/ooapi.*page=[0-9]+");
+    for _repo in "${_repos}"; do
+      git clone $_repo
+    done
+  done
+
+  cd $_cwd;
+
+  return 0;
+}
+
+main $1 $2;
